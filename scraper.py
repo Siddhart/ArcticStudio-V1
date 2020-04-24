c6f59485e732418c4cc668aa6f3ee6d245d895cc
GameFreak21 <GameFreak21@users.noreply.github.com>
2020-04-25 01:21:32 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,36 +19,50 @@ def getFreeGames():
     google.get("https://www.epicgames.com/store/en-US/")
 
     time.sleep(10)
+    name1 = ''
+    imgURL1 = ''
+    freeUntil1 = ''
+    url1 = ''
+    name2 = ''
+    imgURL2 = ''
+    freeUntil2 = ''
+    url2 = ''
+    name3 = ''
+    imgURL3 = ''
+    freeUntil3 = ''
+    url3 = ''
 
-    name1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[1]/div/a/div/div/div[2]/span[1]').text
-    imgURL1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[1]/div/a/div/div/div[1]/div/div/img').get_attribute("data-image")
-    freeUntil1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[1]/div/a/div/div/div[2]/span[2]/span/time[1]').text
-    url1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[1]/div/a').get_attribute("href")
+    try:
+        name1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[1]/div/a/div/div/div[2]/span[1]').text
+        imgURL1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[1]/div/a/div/div/div[1]/div/div/img').get_attribute("data-image")
+        freeUntil1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[1]/div/a/div/div/div[2]/span[2]/span/time[1]').text
+        url1 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[1]/div/a').get_attribute("href")
+    except:
+        print("xpath broke!")
+    
+    try:
+        free2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[2]/div/a/div/div/div[1]/span').text
+        if free2 == "FREE NOW":
+            name2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[2]/div/a/div/div/div[2]/span[1]').text
+            imgURL2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[2]/div/a/div/div/div[1]/div/div/img').get_attribute("data-image")
+            freeUntil2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[2]/div/a/div/div/div[2]/span[2]/span/time[1]').text
+            url2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[2]/div/a').get_attribute("href")
+        else:
+            print('no second offer!')
+    except:
+        print("xpath broke!")
 
-    print("no third offer!")
-    free2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[2]/div/a/div/div/div[1]/span').text
-    if free2 == "FREE NOW":
-        name2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[2]/div/a/div/div/div[2]/span[1]').text
-        imgURL2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[2]/div/a/div/div/div[1]/div/div/img').get_attribute("data-image")
-        freeUntil2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[2]/div/a/div/div/div[2]/span[2]/span/time[1]').text
-        url2 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[2]/div/a').get_attribute("href")
-    else:
-        name2 = ""
-        imgURL2 = ""
-        freeUntil2 = ""
-        url2 = ""
-
-    free3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[3]/div/a/div/div/div[1]/span').text
-    if free3 == "FREE NOW":
-        name3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[3]/div/a/div/div/div[2]/span[1]').text
-        imgURL3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[3]/div/a/div/div/div[1]/div/div[1]/img').get_attribute("data-image")
-        freeUntil3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[3]/div/a/div/div/div[2]/span[2]/span/time[1]').text
-        url3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/div[3]/div/span/div/section/div/div[3]/div/a').get_attribute("href")
-    else:
-        name3 = ""
-        imgURL3 = ""
-        freeUntil3 = ""
-        url3 = ""
+    try:
+        free3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[3]/div/a/div/div/div[1]/span').text
+        if free3 == "FREE NOW":
+            name3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[3]/div/a/div/div/div[2]/span[1]').text
+            imgURL3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[3]/div/a/div/div/div[1]/div/div/img').get_attribute("data-image")
+            freeUntil3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[3]/div/a/div/div/div[2]/span[2]/span/time[1]').text
+            url3 = google.find_element_by_xpath('//*[@id="dieselReactWrapper"]/div/div[4]/main/div/div/div/div/section[2]/div/span/div/section/div/div[3]/div/a').get_attribute("href")
+        else:
+            print('no third offer!')
+    except:
+        print("xpath broke!")
 
     x = {
         "games":
@@ -64,4 +78,4 @@ def getFreeGames():
     return realJson
 
 if __name__ == "__main__":
-    app.run(port=88)
+    app.run(port=88, host='192.168.178.24')
